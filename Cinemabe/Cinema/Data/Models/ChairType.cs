ba11f20a8194a761e989630d5e0724cb5a867c1d
Phạm Viết Tường <113772101+PhamVietTuong@users.noreply.github.com>
2024-03-15 12:27:51 +0700

    first commit

---
+++ Cinemabe/Cinema/Data/Models/ChairType.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinemabe.Data.Models
+{
+	public class ChairType
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool Status { get; set; }
+    }
+}
