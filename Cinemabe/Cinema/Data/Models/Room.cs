4f40d3a48898bc1046fb7c1103e7dac7abb0fa21
Phạm Viết Tường <113772101+PhamVietTuong@users.noreply.github.com>
2024-03-18 21:52:51 +0700

    edit model Room add with and length

--- Cinemabe/Cinema/Data/Models/Room.cs
+++ Cinemabe/Cinema/Data/Models/Room.cs
@@ -4,11 +4,15 @@
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
-
         public int TheaterId { get; set; }
         public Theater Theater { get; set; }
 
-        public bool Status { get; set; }
+		public string Name { get; set; }
+
+		public double With { get; set; }
+
+		public double Length { get; set; }
+
+		public bool Status { get; set; }
     }
 }
