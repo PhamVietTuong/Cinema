c339982cd085aa05c9596a1ebd37299bd6fa8680
Nhuy11 <Nhuynguyen.290720@gmail.com>
2024-06-25 14:07:50 +0700

    commit date 25/6

--- cinema_app/lib/views/Account/login_screen.dart
+++ cinema_app/lib/views/Account/login_screen.dart
@@ -17,105 +17,117 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new),
+          color: Styles.boldTextColor["dark_purple"], // Màu bạn muốn
+          onPressed: () {
+            Navigator.pop(this.context);
+          },
+        ),
+        title: Text(
+          "Đăng nhập",
+          style: TextStyle(
+            fontSize: Styles.appbarFontSize,
+            color: Styles.boldTextColor["dark_purple"],
+          ),
+        ),
+        backgroundColor: Styles.backgroundContent["dark_purple"],
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              "Đăng nhập",
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              decoration: InputDecoration(
-                filled: true,
-                labelText: 'Email đăng nhập',
-                prefixIcon: const Icon(Icons.email),
-                contentPadding: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 10.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                  borderSide: const BorderSide(color: Colors.transparent),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                  borderSide: const BorderSide(color: Colors.transparent),
-                ),
-              ),
-              controller: email,
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              decoration: InputDecoration(
-                filled: true,
-                labelText: 'Mật khẩu',
-                prefixIcon: const Icon(Icons.password),
-                contentPadding: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 10.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                  borderSide: const BorderSide(color: Colors.transparent),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                  borderSide: const BorderSide(color: Colors.transparent),
-                ),
-              ),
-              obscureText: true,
-              controller: pass,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                    onPressed: () {},
-                    child: const Text("Quên mật khẩu",
-                        style: TextStyle(fontSize: 16))),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text(
-                'Đăng nhập',
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  "Người dùng mới!",
-    
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                OutlinedButton(
-                  onPressed: () {
-                     Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const RegistrationScreen()),
-                    );
-                  },
-                  style: OutlinedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
+      body: Container(
+        color: Styles.backgroundColor["dark_purple"],
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.symmetric(
+                horizontal: Styles.defaultHorizontal),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelText: 'Tên đăng nhập',
+                    prefixIcon: const Icon(Icons.email),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 10.0),
+                   
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    //  borderSide: const BorderSide(color: Colors.transparent),
                     ),
+                    labelStyle: const TextStyle( fontSize: Styles.titleFontSize)
                   ),
-                  child: const Text(
-                    "Đăng ký",
+                  controller: email,
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  decoration:  InputDecoration(
+                    filled: true,
+                    labelText: 'Mật khẩu',
+                    prefixIcon: const Icon(Icons.password),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 10.0),
+                     enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      //borderSide: const BorderSide(color: Colors.transparent),
+                    ),
+                    labelStyle: const TextStyle( fontSize: Styles.titleFontSize)
                    
                   ),
+                  obscureText: true,
+                  controller: pass,
                 ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                        onPressed: () {},
+                        child:  Text("Quên mật khẩu",
+                            style: TextStyle(fontSize: Styles.textSize,color: Styles.textColor["dark_purple"]))),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Đăng nhập',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text(
+                      "Người dùng mới!",
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const RegistrationScreen()),
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      child: const Text(
+                        "Đăng ký",
+                      ),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
